1b8fa6a5de34120d402f2fb08d7d8e9b11754ac3
Michell Cronberg <michell@cronberg.dk>
2026-09-21 23:27:44 +0200

    refactor: adjust formatting in BallTests for improved readability

--- tests/Pong.Tests/BallTests.cs
+++ tests/Pong.Tests/BallTests.cs
@@ -7,52 +7,52 @@ namespace Pong.Tests;
 
 public class BallTests
 {
-    [Theory]
-    [InlineData(800f, 600f, 394f, 294f)]
-    [InlineData(1025f, 769f, 506.5f, 378.5f)]
-    public void NewBall_StartsCenteredAndStationary(
-        float screenWidth, float screenHeight, float expectedX, float expectedY)
-    {
-        var ball = new Ball(screenWidth, screenHeight);
+  [Theory]
+  [InlineData(800f, 600f, 394f, 294f)]
+  [InlineData(1025f, 769f, 506.5f, 378.5f)]
+  public void NewBall_StartsCenteredAndStationary(
+      float screenWidth, float screenHeight, float expectedX, float expectedY)
+  {
+    var ball = new Ball(screenWidth, screenHeight);
 
-        Assert.Equal(new Vector2(expectedX, expectedY), ball.Position);
-        Assert.Equal(Vector2.Zero, ball.Velocity);
-    }
+    Assert.Equal(new Vector2(expectedX, expectedY), ball.Position);
+    Assert.Equal(Vector2.Zero, ball.Velocity);
+  }
 
-    [Theory]
-    [InlineData(1)]
-    [InlineData(2)]
-    public void PlaceAtCenter_AfterMovement_CentersAndStopsBall(int towardsPlayer)
-    {
-        var ball = new Ball(800f, 600f);
-        var leftPaddle = new Paddle(20f, 600f, Keys.W, Keys.S);
-        var rightPaddle = new Paddle(766f, 600f, Keys.Up, Keys.Down);
-        var center = new Vector2(394f, 294f);
-        ball.Launch(towardsPlayer);
-        ball.Update(0.1f, leftPaddle, rightPaddle);
-        Assert.NotEqual(center, ball.Position);
-        Assert.NotEqual(Vector2.Zero, ball.Velocity);
+  [Theory]
+  [InlineData(1)]
+  [InlineData(2)]
+  public void PlaceAtCenter_AfterMovement_CentersAndStopsBall(int towardsPlayer)
+  {
+    var ball = new Ball(800f, 600f);
+    var leftPaddle = new Paddle(20f, 600f, Keys.W, Keys.S);
+    var rightPaddle = new Paddle(766f, 600f, Keys.Up, Keys.Down);
+    var center = new Vector2(394f, 294f);
+    ball.Launch(towardsPlayer);
+    ball.Update(0.1f, leftPaddle, rightPaddle);
+    Assert.NotEqual(center, ball.Position);
+    Assert.NotEqual(Vector2.Zero, ball.Velocity);
 
-        ball.PlaceAtCenter();
+    ball.PlaceAtCenter();
 
-        Assert.Equal(center, ball.Position);
-        Assert.Equal(Vector2.Zero, ball.Velocity);
-    }
+    Assert.Equal(center, ball.Position);
+    Assert.Equal(Vector2.Zero, ball.Velocity);
+  }
 
-    [Theory]
-    [InlineData(1, -1)]
-    [InlineData(2, 1)]
-    public void Launch_ServesTowardsPlayerAtBaseSpeed(
-        int towardsPlayer, int expectedHorizontalDirection)
-    {
-        var ball = new Ball(800f, 600f);
-        var positionBeforeLaunch = ball.Position;
+  [Theory]
+  [InlineData(1, -1)]
+  [InlineData(2, 1)]
+  public void Launch_ServesTowardsPlayerAtBaseSpeed(
+      int towardsPlayer, int expectedHorizontalDirection)
+  {
+    var ball = new Ball(800f, 600f);
+    var positionBeforeLaunch = ball.Position;
 
-        ball.Launch(towardsPlayer);
+    ball.Launch(towardsPlayer);
 
-        Assert.Equal(expectedHorizontalDirection, Math.Sign(ball.Velocity.X));
-        Assert.InRange(ball.Velocity.Length(), Ball.BaseSpeed - 0.001f, Ball.BaseSpeed + 0.001f);
-        Assert.NotEqual(0f, ball.Velocity.Y);
-        Assert.Equal(positionBeforeLaunch, ball.Position);
-    }
+    Assert.Equal(expectedHorizontalDirection, Math.Sign(ball.Velocity.X));
+    Assert.InRange(ball.Velocity.Length(), Ball.BaseSpeed - 0.001f, Ball.BaseSpeed + 0.001f);
+    Assert.NotEqual(0f, ball.Velocity.Y);
+    Assert.Equal(positionBeforeLaunch, ball.Position);
+  }
 }
